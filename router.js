8ef100ec4ec6f72f2e7ad2012118bf14b4489312
krunal333 <53342447+krunal333@users.noreply.github.com>
2020-10-10 19:53:47 +0530

    Add files via upload

---
+++ router.js
@@ -0,0 +1,9 @@
+const { response } = require('express');
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send('server is up and runnig');
+});
+
+module.exports = router;
